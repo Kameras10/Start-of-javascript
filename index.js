b8938fba3db923edba444c0cf12a2db7f521698b
Kameras10 <kamerasss10@gmail.com>
2021-12-19 06:10:58 +0000

    ....

--- index.js
+++ index.js
@@ -36,6 +36,33 @@ const calcArea = radius => 3.14 * radius**2; // this only works when there is on
 //     return 3.14 * radius**2 
 // }; //arrow function version of regular function / just a newer and shorter way to write functions
 
-const area = calcArea(5);
-console.log(`area is:`, area);
- 
+// const area = calcArea(5);
+// console.log(`area is:`, area);
+
+
+                    // practise arrow functions
+// // const greet = function(){
+// //     return 'hello, world'
+// // };
+// const greet = () => 'hello, world'
+// const test = greet();
+// console.log(test);
+
+// // const greet = console.log('hello, world'); //          ----------
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+const bill = (products, tax) => {
+let total = 0;
+for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+}
+return total;
+};
+
+console.log(bill([10,15,30], 0.2))
