0f99cae40496446886fc671b342a86b022808fa3
Kameras10 <kamerasss10@gmail.com>
2021-12-19 04:10:31 +0000

    .........

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// function greet(name, lastName, age){
-//     console.log("Hello " + name + lastName + age)
+// Object - Arrays, Object Literals, Functions, Dates etc
+// Function does particular thing 
+// Defining the code inside the function once but can rut it multiply times
+
+// function greet(name, lastName, age, city, question){
+//     console.log("Hello " + name + lastName + age + city + question);
 // }
-// greet ("Josh ", "Irwin ", "22")
+// greet("Josh ", "Survival ", "22 ", "Michigan ", "How was your journey?");
+// greet("Kentucky ", "Ubguva ", "42 ", "Kentuccky ", "How was your journey?");
+
+
